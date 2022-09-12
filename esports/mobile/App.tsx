ea27b87e8fa87b0094a41ed993fc7bf4497f277a
Matheus Barbosa <ps.matheusb@gmail.com>
2022-09-12 10:17:29 -0300

    initial mobile interface

--- esports/mobile/App.tsx
+++ esports/mobile/App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+
+interface ButtonProps{
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text>
+      </Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Aoba Pessoar</Text>
+      <StatusBar style="auto" backgroundColor='cyan'/>
+      <Button title='Olá'/>
     </View>
   );
 }
@@ -13,8 +28,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8528ac',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title:{
+    color:'#fff',
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+
+  button:{
+    margin:'10px',
+    color:'#000',
+    backgroundColor: '#fff'
+  }
 });
